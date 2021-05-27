a6a136ea31ead9791991302486886d4a1d52a8a1
domingobishop <domingobishop@gmail.com>
2021-05-27 16:54:53 +0100

    rewrite admin and site URL based on HTTP_X_FORWARDED_HOST

--- ds-tna-wp-aws.php
+++ ds-tna-wp-aws.php
@@ -18,5 +18,5 @@ add_action( 'admin_menu', 'tna_aws_add_menu_item' );
 
 /* add_filter */
 add_filter('allowed_redirect_hosts', 'add_redirect_hosts', 10);
-add_filter('login_redirect', 'optional_login_redirect', 10, 3);
-add_filter( 'site_url', 'forwarded_site_url', 10, 1 );
+add_filter( 'admin_url', 'forwarded_admin_url', 10, 2 );
+add_filter( 'site_url', 'forwarded_site_url', 10, 2 );
