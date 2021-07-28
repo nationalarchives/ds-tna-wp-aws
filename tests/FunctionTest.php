0a8832b3b4e14673e090e1fbced55262fefb3496
Chris Bishop <domingobishop@users.noreply.github.com>
2021-07-28 14:11:24 +0100

    url rewrite tests

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -10,8 +10,38 @@ final class FunctionTest extends TestCase
         $this->assertFalse(false);
     }
 
-    public function test_exists_tna_styles()
+    public function test_add_redirect_hosts()
     {
         $this->assertTrue(function_exists('add_redirect_hosts'));
+    }
+
+    public function test_forwarded_attachments_url()
+    {
+        $returns = forwarded_attachments_url( 'http://internal.com' );
+        $this->assertEquals($returns, 'https://editorial.com');
+    }
+
+    public function test_forwarded_site_url()
+    {
+        $returns = forwarded_site_url( 'http://internal.com' );
+        $this->assertEquals($returns, 'https://editorial.com');
+    }
+
+    public function test_forwarded_network_domain()
+    {
+        $network = new stdClass();
+        $network->domain = 'internal.com';
+        $network->cookie_domain = 'internal.com';
+        $returns = forwarded_network_domain( $network );
+        $this->assertEquals($returns->domain, 'editorial.com');
+    }
+
+    public function test_forwarded_network_domain_cookie()
+    {
+        $network = new stdClass();
+        $network->domain = 'internal.com';
+        $network->cookie_domain = 'internal.com';
+        $returns = forwarded_network_domain( $network );
+        $this->assertEquals($returns->cookie_domain, 'editorial.com');
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require dirname( __DIR__ ) . '/functions.php';
+require dirname( __DIR__ ) . '/src/testHelper.php';
 
 // Enable Composer autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
